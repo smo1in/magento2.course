ddfd81e904f85fcbef73740e1438e8cb479bcbc4
smo1in <smolingm@gmail.com>
2020-06-16 19:13:16 +0700

    addFilterByGroup

--- app/code/Vendor/Voucher/Plugin/PrivilegedCustomer.php
+++ app/code/Vendor/Voucher/Plugin/PrivilegedCustomer.php
@@ -4,43 +4,42 @@ namespace Vendor\Voucher\Plugin;
 
 use Exception;
 use Magento\Customer\Model\CustomerFactory as CustomerModelFactory;
+use Magento\Customer\Model\ResourceModel\Group\CollectionFactory as GroupCollectionFactory;
 use Vendor\Voucher\Model\VoucherManagement as Subject;
-
 
 class PrivilegedCustomer
 {
-
     /**@var CustomerModelFactory */
     private $customerModelFactory;
 
-    /**
-     * @param CustomerModelFactory $customerModelFactory
-     */
+    /** @var GroupCollectionFactory */
+    private $groupCollectionFactory;
 
-    public function __construct(CustomerModelFactory $customerModelFactory)
-    {
+    public function __construct(
+        CustomerModelFactory $customerModelFactory,
+        GroupCollectionFactory $groupCollectionFactory
+    ) {
         $this->customerModelFactory = $customerModelFactory;
+        $this->groupCollectionFactory = $groupCollectionFactory;
     }
 
     /**
      * @param Subject $subject
-     * @param $id
+     * @param int $customer_Id
+     * @return int
      * @throws Exception
      */
-    public function beforeCreateVoucher(Subject $subject, $id)
+    public function beforeCreateVoucher(Subject $subject, int $customer_id)
     {
+        $group = $this->groupCollectionFactory->create()
+            ->addFieldToFilter('customer_group_code', 'Privileged Customers')
+            ->getFirstItem();
 
-        $customerGroupId = $this->customerModelFactory->create()->load($id)->getGroupId();
+        $customer = $this->customerModelFactory->create()->load($customer_id);
 
-        if (!$customerGroupId = 4) {
+        if ($group->getId() != $customer->getGroupId()) {
             throw new Exception("Customer is not in a privileged group");
         }
 
     }
 }
-
-
-
-
-
-
